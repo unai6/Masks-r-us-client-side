54d44348b7d367f069ffc1a9db0dee564be3ff29
Adrian Vide <adrian.vide.paris@gmail.com>
2020-04-16 10:16:08 +0200

    carousel

--- src/urls/Home.js
+++ src/urls/Home.js
@@ -3,50 +3,128 @@ import MainList from "../components/MainList";
 
 function Home() {
   return (
-    <div>
-      <div
-        id="carouselFadeExampleIndicators"
-        class="carousel slide carousel-fade"
-        data-ride="carousel"
-      >
-        <div class="carousel-inner" role="listbox">
-          <div class="carousel-item active">
-            <img
-              class="d-block w-100"
-              src="#"
-              data-src="holder.js/900x400?theme=social"
-              alt="First slide"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              class="d-block w-100"
-              src="#"
-              data-src="holder.js/900x400?theme=industrial"
-              alt="Second slide"
-            />
+    <div
+      id="carouselExampleIndicators"
+      className="carousel slide d-flex"
+      data-ride="carousel"
+    >
+      <ol className="carousel-indicators">
+        <li
+          data-target="#carouselExampleIndicators"
+          data-slide-to="0"
+          className="active"
+        ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            className="d-block w-100"
+            src="https://ae01.alicdn.com/kf/Hc32e6c42971a45609dcf8cd083ba3b404/7pcs-Organic-Vapor-Full-Face-Respirator-Mask-Gas-Mask-Paint-Pesticide-Chemical-Formaldehyde-Anti-Virus-Respiratory.jpg"
+            alt="First slide"
+          />
+          <div className="carousel-caption">
+            <h5 className="carousel-R-text">MASK-NAME</h5>
+            <p className="carousel-R-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel,
+              dolore. Ducimus consequuntur, 
+            </p>
           </div>
         </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselFadeExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselFadeExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+        <div className="carousel-item">
+          <img
+            className="d-block w-100"
+            src="https://ae01.alicdn.com/kf/H1e8b1309bf3b418ba9d3eb366fbdec159/3600-Half-Face-Gas-Mask-Particulate-Respirator-with-Filter-Cartridge-Protective-Mask-Personal-Protective-Equippment-dust.jpg"
+            alt="Second slide"
+          />
+          <div className="carousel-caption">
+            <h5 className="carousel-R-text">MASK-NAME</h5>
+            <p className="carousel-R-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel,
+              dolore. Ducimus consequuntur, cum ex illum blanditiis dolor
+              distinctio dignissimos 
+            </p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img
+            className="d-block w-100"
+            src="https://ae01.alicdn.com/kf/H2ae29a535056477f985635b3ce36ff304/Unsex-Masks-Antidust-Mouth-Face-Mask-Muzzle-Pm2-5-Antibacterial-Outdoor-Trip-Protection-Mouth-Mask-Mascarillas.jpg"
+            alt="Third slide"
+          />
+          <div className="carousel-caption">
+            <h5 className="carousel-R-text">MASK-NAME</h5>
+            <p className="carousel-R-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel,
+              dolore. Ducimus consequuntur, 
+            </p>
+          </div>
+        </div>
       </div>
+      <a
+        className="carousel-control-prev "
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only ">Previous</span>
+      </a>
+      <a
+        className="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
     </div>
+    // <div>
+    //   <div
+    //     id="carouselFadeExampleIndicators"
+    //     className="carousel slide carousel-fade"
+    //     data-ride="carousel"
+    //   >
+    //     <div className="carousel-inner" role="listbox">
+    //       <div className="carousel-item active">
+    //         <img
+    //           className="d-block w-100"
+    //           src="#"
+    //           data-src="holder.js/900x400?theme=social"
+    //           alt="First slide"
+    //         />
+    //       </div>
+    //       <div className="carousel-item">
+    //         <img
+    //           className="d-block w-100"
+    //           src="#"
+    //           data-src="holder.js/900x400?theme=industrial"
+    //           alt="Second slide"
+    //         />
+    //       </div>
+    //     </div>
+    //     <a
+    //       className="carousel-control-prev"
+    //       href="#carouselFadeExampleIndicators"
+    //       role="button"
+    //       data-slide="prev"
+    //     >
+    //       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    //       <span className="sr-only">Previous</span>
+    //     </a>
+    //     <a
+    //       className="carousel-control-next"
+    //       href="#carouselFadeExampleIndicators"
+    //       role="button"
+    //       data-slide="next"
+    //     >
+    //       <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    //       <span className="sr-only">Next</span>
+    //     </a>
+    //   </div>
+    // </div>
   );
 }
 
